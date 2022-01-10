92fbf65e90323ba0f4c8ac1cc3b81ea1aaf4a8c9
Ajuaman <tate.chin24@gmail.com>
2022-01-10 13:01:30 -0500

    What I've Got So Far

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,70 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
+  float circleSpeed = 5;
+  float[] circleY = new float[25];
+  float rectHeight = 5;
+
+  int num = 25;
+  int[] x = new int[num];
+  int[] y = new int[num];
+  int indexPosition = 0;
+
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    for (int i = 0; i < circleY.length; i++) {
+      circleY[i] = random(height);
+    }
+
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    background(50);
+    for(int j = 0; j < 400; j+=5){
+      rect(j, 395, 5, 5);
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    for (int i = 0; i < circleY.length; i++) {
+      float circleX = width * i / circleY.length;
+      ellipse(circleX, circleY[i], 5, rectHeight);
+
+      circleY[i]+= circleSpeed;
+
+      if (circleY[i] > height) {
+        circleY[i] = 0;
+      }
+    }
+
+    noStroke();
+    x[indexPosition] = mouseX;
+    y[indexPosition] = mouseY;
+    indexPosition = (indexPosition + 1) % num;
+    for (int i = 0; i < num; i++) {
+      
+      int pos = (indexPosition + i) % num;
+      float radius = (num-i) / 2;
+      ellipse(x[pos], y[pos], radius, radius);
+    }
   }
-  
-  // define other methods down here.
+
+  @Override
+  public void keyPressed() {
+      // TODO Auto-generated method stub
+      super.keyPressed();
+      if(keyCode == DOWN){
+        circleSpeed = 3;
+      }
+      else if(keyCode == UP){
+        circleSpeed = 7;
+      }
+  }
+
+  @Override
+    public void keyReleased() {
+        circleSpeed = 5;
+    }
 }
